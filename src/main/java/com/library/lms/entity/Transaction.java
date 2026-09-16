28a889b49a24f760a4e69fa1cfbe5f447c44bc96
suyash-pratuyash <pratuyashsuyash@gmail.com>
2026-09-16 22:17:34 +0530

    add Transaction and TransactionStatus entities for library management system

---
+++ src/main/java/com/library/lms/entity/Transaction.java
@@ -0,0 +1,42 @@
+package com.library.lms.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "transactions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "borrow_date", nullable = false)
+    private LocalDate borrowDate;
+
+    @Column(name = "due_date", nullable = false)
+    private LocalDate dueDate;
+
+    @Column(name = "return_date")
+    private LocalDate returnDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionStatus status;
+}
---
+++ src/main/java/com/library/lms/entity/TransactionStatus.java
@@ -0,0 +1,7 @@
+package com.library.lms.entity;
+
+public enum TransactionStatus {
+    BORROWED,
+    RETURNED,
+    OVERDUE
+}
