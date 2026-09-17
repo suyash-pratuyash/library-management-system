e32d48102762355177515e4742ea37994990e132
suyash-pratuyash <pratuyashsuyash@gmail.com>
2026-09-17 21:14:14 +0530

    add method to BookRepository for searching books by title or author

--- src/main/java/com/library/lms/repository/BookRepository.java
+++ src/main/java/com/library/lms/repository/BookRepository.java
@@ -3,6 +3,7 @@ package com.library.lms.repository;
 import com.library.lms.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository  extends JpaRepository<Book, Long>
@@ -10,4 +11,7 @@ public interface BookRepository  extends JpaRepository<Book, Long>
     Optional<Book> findByIsbn(String isbn);
 
     boolean existsByIsbn(String isbn);
+
+    // add to BookRepository
+    List<Book> findByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(String title, String author);
 }
